1691fe7e23e14ea5732eda86132766a9ea6244b5
Chinseok Lee <me@askcompany.kr>
2020-02-16 01:12:32 +0900

    로그인-로그아웃 구현 그리고 회원 가입과 동시에 로그인

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('login/', views.login, name='login'),  # /accounts/login/ => settings.LOGIN_URL
+    path('logout/', views.logout, name='logout'),
     path('signup/', views.signup, name='signup'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,16 @@
 from django.contrib import messages
+from django.contrib.auth import login as auth_login
+from django.contrib.auth.views import LoginView, logout_then_login
 from django.shortcuts import redirect, render
 from .forms import SignupForm
+
+
+login = LoginView.as_view(template_name="accounts/login_form.html")
+
+
+def logout(request):
+    messages.success(request, '로그아웃되었습니다.')
+    return logout_then_login(request)
 
 
 def signup(request):
@@ -8,6 +18,7 @@ def signup(request):
         form = SignupForm(request.POST)
         if form.is_valid():
             signed_user = form.save()
+            auth_login(request, signed_user)
             messages.success(request, "회원가입 환영합니다.")
             signed_user.send_welcome_email()  # FIXME: Celery로 처리하는 것을 추천.
             next_url = request.GET.get('next', '/')
