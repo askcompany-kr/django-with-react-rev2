5fcff91fd92fc8341f6256eb3bf91dc2c03bfa30
Chinseok Lee <me@askcompany.kr>
2020-02-16 01:23:11 +0900

    django-pydenticon 콘을 활용하여 프로필 기본 이미지 구현

--- askcompany/settings/common.py
+++ askcompany/settings/common.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     # Third Apps
     'bootstrap4',
     'debug_toolbar',
+    'django_pydenticon',
     # Locals Apps
     'accounts',
 ]
--- askcompany/urls.py
+++ askcompany/urls.py
@@ -4,10 +4,12 @@ from django.contrib import admin
 from django.contrib.auth.decorators import login_required
 from django.urls import include, path
 from django.views.generic import TemplateView
+from django_pydenticon.views import image as pydenticon_image
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
+    path('identicon/image/<path:data>/', pydenticon_image,name='pydenticon_image'),
     path('', login_required(TemplateView.as_view(template_name='root.html')), name='root'),
 ]
 
