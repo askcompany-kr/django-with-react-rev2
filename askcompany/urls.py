2bb0730eae9583da9c64754304c054ebea38a862
Chinseok Lee <me@askcompany.kr>
2020-02-15 21:29:19 +0900

    bootstrap4를 활용한 기본 레이아웃 구현

--- askcompany/urls.py
+++ askcompany/urls.py
@@ -2,9 +2,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='root.html'), name='root'),
 ]
 
 if settings.DEBUG:
