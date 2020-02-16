0d6065c64bfaf9066d3211dd4299c2c42e67c3e2
Chinseok Lee <me@askcompany.kr>
2020-02-16 11:28:59 +0900

    Timeline에 Follow하는 유저들의 포스팅 만을 노출

--- instagram/views.py
+++ instagram/views.py
@@ -1,6 +1,7 @@
 from django.contrib import messages
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect, render
 from .forms import PostForm
 from .models import Post
@@ -8,10 +9,17 @@ from .models import Post
 
 @login_required
 def index(request):
+    post_list = Post.objects.all()\
+        .filter(
+            Q(author=request.user) |
+            Q(author__in=request.user.following_set.all())
+        )
+
     suggested_user_list = get_user_model().objects.all()\
         .exclude(pk=request.user.pk)\
         .exclude(pk__in=request.user.following_set.all())[:3]
     return render(request, "instagram/index.html", {
+        "post_list": post_list,
         "suggested_user_list": suggested_user_list,
     })
 
