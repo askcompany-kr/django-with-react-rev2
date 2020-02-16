c3adfc673ae76d37af9e1cb6359211468f269b64
Chinseok Lee <me@askcompany.kr>
2020-02-16 12:49:23 +0900

    유저페이지에서 Unfollow 구현, 그리고 Timeline 포스팅 조건에 기간 제한 걸기

--- instagram/models.py
+++ instagram/models.py
@@ -4,7 +4,15 @@ from django.db import models
 from django.urls import reverse
 
 
-class Post(models.Model):
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Post(BaseModel):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     photo = models.ImageField(upload_to="instagram/post/%Y/%m/%d")
     caption = models.CharField(max_length=500)
--- instagram/views.py
+++ instagram/views.py
@@ -1,18 +1,24 @@
+from datetime import timedelta
 from django.contrib import messages
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect, render
+from django.utils import timezone
 from .forms import PostForm
 from .models import Post
 
 
 @login_required
 def index(request):
+    timesince = timezone.now() - timedelta(days=3)
     post_list = Post.objects.all()\
         .filter(
             Q(author=request.user) |
             Q(author__in=request.user.following_set.all())
+        )\
+        .filter(
+            created_at__gte=timesince
         )
 
     suggested_user_list = get_user_model().objects.all()\
@@ -54,8 +60,15 @@ def user_page(request, username):
     page_user = get_object_or_404(get_user_model(), username=username, is_active=True)
     post_list = Post.objects.filter(author=page_user)
     post_list_count = post_list.count()  # 실제 데이터베이스에 count 쿼리를 던지게 됩니다.
+
+    if request.user.is_authenticated:
+        is_follow = request.user.following_set.filter(pk=page_user.pk).exists()
+    else:
+        is_follow = False
+
     return render(request, "instagram/user_page.html", {
         "page_user": page_user,
         "post_list": post_list,
         "post_list_count": post_list_count,
+        "is_follow": is_follow,
     })
